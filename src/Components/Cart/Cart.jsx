9df17c377494b6a657508c0721d1bae5fe7b4b3d
masudra <masudrana6278312@gmail.com>
2023-03-29 00:15:22 +0600

    summry add

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    const cart =props.cart
+
+    let totalPrice = 0;
+    let totalShip =0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.cProduct.price;
+        totalShip = totalShip + product.cProduct.shipping;
+    }
+    const tax =totalPrice*7/100;
+    const grandtotal = totalPrice + totalShip + tax;
+    return (
+        <div className='cartcss'>
+            <h3> Order Summary</h3>
+                <h4>Cart List : {cart.length}</h4>
+                <h4>Total Price: ${totalPrice}</h4>
+                <h4>ShipingCost : ${totalShip}</h4>
+                <h4>Tex: {tax}</h4>
+                <h4>Grand Total : ${grandtotal}</h4>
+            
+        </div>
+    );
+};
+
+export default Cart;
--- src/Components/Shope/Shop.jsx
+++ src/Components/Shope/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import  './Shop.css'
 
@@ -26,8 +27,7 @@ const Shop = () => {
             </div>
 
             <div>
-                <h3> Order Summary</h3>
-                <p>Cart List : {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
             
         </div>
